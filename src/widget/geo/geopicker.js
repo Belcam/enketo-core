11f78fbaed76caef6074a307554c5511a78e69a3
tom <thomas.demaet@uclouvain.be>
2016-02-08 14:58:48 +0100

    add working WMS (and bing, but not working)

--- src/widget/geo/geopicker.js
+++ src/widget/geo/geopicker.js
@@ -798,11 +798,20 @@ define( function( require, exports, module ) {
         var that = this,
             tasks = [];
 
+console.debug(maps);
         maps.forEach( function( map, index ) {
-            if ( typeof map.tiles === 'string' && /^GOOGLE_(SATELLITE|ROADMAP|HYBRID|TERRAIN)/.test( map.tiles ) ) {
+console.debug(map, index, map.tiles === 'string', /^GOOGLE_(SATELLITE|ROADMAP|HYBRID|TERRAIN)/.test( map.tiles ), /^BING_(Aerial|AerialWithLabels|Birdseye|BirdseyeWithLabels|Road)/.test( map.tiles ));
+            if ( typeof map.tiles === 'string') {
+              if( /^GOOGLE_(SATELLITE|ROADMAP|HYBRID|TERRAIN)/.test( map.tiles ) ) {
                 tasks.push( that._getGoogleTileLayer( map, index ) );
+              } else if( /^BING_(Aerial|AerialWithLabels|Birdseye|BirdseyeWithLabels|Road)/.test( map.tiles ) ) {
+                tasks.push( that._getBingTileLayer( map, index ) );
+              }
             } else
             if ( map.tiles ) {
+              if( map.type == 'WMS' )
+                tasks.push( that._getLeafletTileWMSLayer( map, index ) );
+              else
                 tasks.push( that._getLeafletTileLayer( map, index ) );
             } else {
                 console.error( 'Configuration error for map tiles. Not a valid tile layer: ', map );
@@ -831,6 +840,22 @@ define( function( require, exports, module ) {
     };
 
     /**
+     * Asynchronously (fake) obtains a Leaflet/Mapbox tilelayerWMS
+     *
+     * @param  {{}}     map   map layer as defined in the apps configuration
+     * @param  {number} index the index of the layer
+     * @return {Promise}
+     */
+    Geopicker.prototype._getLeafletTileWMSLayer = function( map, index ) {
+        var options = this._getTileOptions( map, index );
+        options.format = map.format || 'image/png';
+        options.transparent = map.transparent || true;
+        options.layers = map.tiles.join(',');
+        return Promise.resolve( L.tileLayer.wms( map.url, options ) );
+    };
+
+
+    /**
      * Asynchronously obtains a Google Maps tilelayer
      *
      * @param  {{}}     map   map layer as defined in the apps configuration
@@ -845,6 +870,21 @@ define( function( require, exports, module ) {
             .then( function() {
                 return new L.Google( type, options );
             } );
+    };
+
+    /**
+     * Asynchronously obtains a Bing tilelayer
+     *
+     * @param  {{}}     map   map layer as defined in the apps configuration
+     * @param  {number} index the index of the layer
+     * @return {Promise}
+     */
+    Geopicker.prototype._getBingTileLayer = function( map, index ) {
+        var options = this._getTileOptions( map, index ),
+            key = map.key;
+        options.type = map.tiles.substring( 5 );
+console.log(options, key);
+        return Promise.resolve( L.BingLayer( key, options ) );
     };
 
     /**
@@ -1660,6 +1700,142 @@ define( function( require, exports, module ) {
             }
         }
         L.Google.asyncWait = [];
+    };
+
+    /*
+     * Bing layers
+     * from https://github.com/shramov/leaflet-plugins/blob/master/layer/tile/Bing.js
+     */ 
+
+    L.BingLayer = L.TileLayer.extend({
+      options: {
+        subdomains: [0, 1, 2, 3],
+        type: 'Aerial',
+        attribution: 'Bing',
+        culture: ''
+      },
+
+      initialize: function(key, options) {
+        L.Util.setOptions(this, options);
+
+        this._key = key;
+        this._url = null;
+        this._providers = [];
+        this.metaRequested = false;
+      },
+
+      tile2quad: function(x, y, z) {
+        var quad = '';
+        for (var i = z; i > 0; i--) {
+          var digit = 0;
+          var mask = 1 << (i - 1);
+          if ((x & mask) !== 0) digit += 1;
+          if ((y & mask) !== 0) digit += 2;
+          quad = quad + digit;
+        }
+        return quad;
+      },
+
+      getTileUrl: function(p, z) {
+        var zoom = this._getZoomForUrl();
+        var subdomains = this.options.subdomains,
+          s = this.options.subdomains[Math.abs((p.x + p.y) % subdomains.length)];
+        return this._url.replace('{subdomain}', s)
+            .replace('{quadkey}', this.tile2quad(p.x, p.y, zoom))
+            .replace('{culture}', this.options.culture);
+      },
+
+      loadMetadata: function() {
+        if (this.metaRequested) return;
+        this.metaRequested = true;
+        var _this = this;
+        var cbid = '_bing_metadata_' + L.Util.stamp(this);
+        window[cbid] = function (meta) {
+          window[cbid] = undefined;
+          var e = document.getElementById(cbid);
+          e.parentNode.removeChild(e);
+          if (meta.errorDetails) {
+            return;
+          }
+          _this.initMetadata(meta);
+        };
+        var urlScheme = (document.location.protocol === 'file:') ? 'http' : document.location.protocol.slice(0, -1);
+        var url = urlScheme + '://dev.virtualearth.net/REST/v1/Imagery/Metadata/'
+              + this.options.type + '?include=ImageryProviders&jsonp=' + cbid +
+              '&key=' + this._key + '&UriScheme=' + urlScheme;
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        script.id = cbid;
+        document.getElementsByTagName('head')[0].appendChild(script);
+      },
+
+      initMetadata: function(meta) {
+        var r = meta.resourceSets[0].resources[0];
+        this.options.subdomains = r.imageUrlSubdomains;
+        this._url = r.imageUrl;
+        if (r.imageryProviders) {
+          for (var i = 0; i < r.imageryProviders.length; i++) {
+            var p = r.imageryProviders[i];
+            for (var j = 0; j < p.coverageAreas.length; j++) {
+              var c = p.coverageAreas[j];
+              var coverage = {zoomMin: c.zoomMin, zoomMax: c.zoomMax, active: false};
+              var bounds = new L.LatLngBounds(
+                  new L.LatLng(c.bbox[0]+0.01, c.bbox[1]+0.01),
+                  new L.LatLng(c.bbox[2]-0.01, c.bbox[3]-0.01)
+              );
+              coverage.bounds = bounds;
+              coverage.attrib = p.attribution;
+              this._providers.push(coverage);
+            }
+          }
+        }
+        this._update();
+      },
+
+      _update: function() {
+        if (this._url === null || !this._map) return;
+        this._update_attribution();
+        L.TileLayer.prototype._update.apply(this, []);
+      },
+
+      _update_attribution: function() {
+        var bounds = this._map.getBounds();
+        var zoom = this._map.getZoom();
+        for (var i = 0; i < this._providers.length; i++) {
+          var p = this._providers[i];
+          if ((zoom <= p.zoomMax && zoom >= p.zoomMin) &&
+              bounds.intersects(p.bounds)) {
+            if (!p.active && this._map.attributionControl)
+              this._map.attributionControl.addAttribution(p.attrib);
+            p.active = true;
+          } else {
+            if (p.active && this._map.attributionControl)
+              this._map.attributionControl.removeAttribution(p.attrib);
+            p.active = false;
+          }
+        }
+      },
+      
+      onAdd: function(map) {
+        this.loadMetadata();
+        L.TileLayer.prototype.onAdd.apply(this, [map]);
+      },
+
+      onRemove: function(map) {
+        for (var i = 0; i < this._providers.length; i++) {
+          var p = this._providers[i];
+          if (p.active && this._map.attributionControl) {
+            this._map.attributionControl.removeAttribution(p.attrib);
+            p.active = false;
+          }
+        }
+              L.TileLayer.prototype.onRemove.apply(this, [map]);
+      }
+    });
+
+    L.bingLayer = function (key, options) {
+        return new L.BingLayer(key, options);
     };
 
     module.exports = {
